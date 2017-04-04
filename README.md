ba9dbdbf850abcec06a7fca185be06769928f051
HarryOsbourne <harryho@aapt.net.au>
2017-04-04 14:35:06 +0800

    Added doc comments and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# watchdog
+# Watchdog Service
+
+Checks if the CMT website and Confluence are still up.
+
+## Get Started
+
+Gets a local version of the Watchdog Service.
+```bash
+$ git clone https://github.com/Curtin-Motorsport-Team/watchdog \
+	$GOPATH/src/github.com/Curtin-Motorsport-Team/watchdog
+```
+
+Change into the newly created directory.
+
+Complie the Watchdog Service.
+```bash
+$ go build
+```
+
+Run the Watchdog Service.
+Make the `\` or `/` is correct for your OS.
+```bash
+.\watchdog
+```
--- main.go
+++ main.go
@@ -14,28 +14,31 @@ func main() {
 	wg := sync.WaitGroup{}
 	for _, url := range urls {
 		wg.Add(1)
-		go check(url, &wg)
-		
+		go check(url, &wg)		
 	}
 	wg.Wait()
 }
 
+// check will log a urls state and then tell the wait group that it is done.
 func check(url string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	up := Checksite(url)
 	LogState(up, url)
 }
 
+// Checksite will get a respone and error from a url and return the status of it.
+// If any errors and encounterded it is assumed that the site is down.
 func Checksite(url string) bool {
-
 	respone, err := http.Get(url)
+	// TODO: inspect what error the page returns
 	if err != nil {
 		return false
 	}
-
 	return respone.StatusCode == 200
 }
 
+// LogState will log the state of the given url, for now jsut prints to the screen
+// Later on could save to a database.
 func LogState(up bool, url string) {
 	var state string
 	if up {
@@ -43,6 +46,5 @@ func LogState(up bool, url string) {
 	} else {
 		state = "down"
 	}
-
 	log.Printf("%v is %v", url, state)
 }
