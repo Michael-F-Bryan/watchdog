578536fac46fce9bd4b73bf887d038f9da27355f
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-04-10 20:10:18 +0800

    Simplified test cases using tabular tests

--- main_test.go
+++ main_test.go
@@ -18,23 +18,23 @@ func getHTTPServer(statusCode int) *httptest.Server {
 // TestCheckWorkingSite starts up a web server on the local machine then makes
 // sure the `Checksite` function shows it's up
 func TestCheckWorkingSite(t *testing.T) {
-	server := getHTTPServer(http.StatusOK)
-	defer server.Close()
-
-	status := Checksite(server.URL)
-
-	if status.State != StateUp {
-		t.Errorf("Expected StateUp, got %v", status.State)
+	inputs := []struct {
+		Status   int
+		ShouldBe State
+	}{
+		{http.StatusOK, StateUp},
+		{http.StatusNotFound, StateDown},
 	}
-}
 
-func TestCheckDownSite(t *testing.T) {
-	server := getHTTPServer(http.StatusNotFound)
-	defer server.Close()
+	for _, input := range inputs {
+		t.Log(input)
+		server := getHTTPServer(input.Status)
 
-	status := Checksite(server.URL)
+		status := Checksite(server.URL)
 
-	if status.State != StateDown {
-		t.Errorf("Expected StateDown, got %v", status.State)
-    }
+		if status.State != input.ShouldBe {
+			t.Errorf("Expected %v, got %v", input.ShouldBe, status.State)
+		}
+		server.Close()
+	}
 }
